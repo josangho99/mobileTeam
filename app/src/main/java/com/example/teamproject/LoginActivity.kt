df4e5e30fb84f496cf8fec82d57dda78210c8230
DESKTOP-3URSLJ2\josangho <josangho99@gmail.com>
2023-05-21 23:37:31 +0900

    메니페스트 Activity 추가 로그인에 회원가입버튼 화면전환 intent추가

--- app/src/main/java/com/example/teamproject/LoginActivity.kt
+++ app/src/main/java/com/example/teamproject/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.teamproject
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.example.teamproject.databinding.ActivityLoginBinding
 
@@ -10,5 +11,10 @@ class LoginActivity : Activity(){
         super.onCreate(savedInstanceState)
         var binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.loginJoinBtn.setOnClickListener {
+            var intent_join = Intent(applicationContext, JoinmembershipActivity::class.java)
+            startActivity(intent_join)
+        }
     }
 }
