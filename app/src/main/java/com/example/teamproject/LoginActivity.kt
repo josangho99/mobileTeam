c9e781089ee9e5735972eff8227e039f3bd0c878
K_Ho <ysking463@gmail.com>
2023-05-31 12:01:14 +0900

    카카오 로그인 구현

--- app/src/main/java/com/example/teamproject/LoginActivity.kt
+++ app/src/main/java/com/example/teamproject/LoginActivity.kt
@@ -1,13 +1,19 @@
 package com.example.teamproject
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.teamproject.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 
 
 class LoginActivity : AppCompatActivity() {
@@ -19,12 +25,19 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         var view = binding.root
         setContentView(view)
+
         auth = FirebaseAuth.getInstance()
+        KakaoSdk.init(this,BuildConfig.KAKAO_NATIVE_APP_KEY)
+
+
         binding.loginLoginBtn.setOnClickListener {
             signInEmail(binding.loginId.text.toString(),binding.loginPw.text.toString())
         }
         binding.loginJoinBtn.setOnClickListener {
             startActivity(Intent(applicationContext,JoinmembershipActivity::class.java))
+        }
+        binding.loginKakaologinBtn.setOnClickListener {
+            kakaologin()
         }
     }
 
@@ -62,5 +75,42 @@ class LoginActivity : AppCompatActivity() {
         super.onStart()
         moveMainPage(auth?.currentUser)
     }
+
+    fun kakaologin(){
+        val callback: (
+            OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.e(TAG, "카카오계정으로 로그인 실패", error)
+            } else if (token != null) {
+                Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+            }
+        }
+
+// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(applicationContext)) {
+            UserApiClient.instance.loginWithKakaoTalk(applicationContext) { token, error ->
+                if (error != null) {
+                    Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(applicationContext, callback = callback)
+                } else if (token != null) {
+                    Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                    moveMainPage(auth?.currentUser)
+
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(applicationContext, callback = callback)
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/example/teamproject/MainActivity.kt
+++ app/src/main/java/com/example/teamproject/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.teamproject
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.teamproject.databinding.ActivityMainBinding
 import android.content.Intent
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.user.UserApiClient
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,12 +22,25 @@ class MainActivity : AppCompatActivity() {
         var binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
+        KakaoSdk.init(this,BuildConfig.KAKAO_NATIVE_APP_KEY)
 
         binding.logoutbtn.setOnClickListener {
             val intent = Intent(this, NaviActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
             auth?.signOut()
+        }
+        binding.kakaoLogoutBtn.setOnClickListener {
+            // 로그아웃
+            UserApiClient.instance.logout { error ->
+                if (error != null) {
+                    Log.e(TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
+                }
+                else {
+                    Log.i(TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
+                }
+            }
+
         }
     }
 }
