9a509a39cf19fdddfe52c8669178c3d92ae699f3
DongHyeon <100687971+rlagus720@users.noreply.github.com>
2023-06-24 05:43:08 +0900

    Add files via upload

--- app/src/main/java/com/example/teamproject/map.kt
+++ app/src/main/java/com/example/teamproject/map.kt
@@ -5,6 +5,11 @@ import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
@@ -24,6 +29,10 @@ import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 import net.daum.mf.map.n.api.internal.NativeMapLocationManager.setShowCurrentLocationMarker
+import org.json.JSONArray
+import org.json.JSONException
+import java.io.IOException
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -86,6 +95,8 @@ class map : Fragment() {
             }
         }
 
+        parseJSONData(requireContext(), mapView)
+
         return binding.root
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_map, container, false)
@@ -109,5 +120,80 @@ class map : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
+        data class Facility(
+            val name: String,
+            val sport: String,
+            val address: String,
+            var latitude: Double,
+            var longitude: Double
+        )
+
+        fun drawableToBitmap(drawable: Drawable?): Bitmap? {
+            if (drawable is BitmapDrawable) {
+                return drawable.bitmap
+            }
+
+            val bitmap = Bitmap.createBitmap(drawable?.intrinsicWidth ?: 0, drawable?.intrinsicHeight ?: 0, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            drawable?.setBounds(0, 0, canvas.width, canvas.height)
+            drawable?.draw(canvas)
+            return bitmap
+        }
+
+        fun parseJSONData(context: Context, mapView: MapView) {
+            val facilityList = mutableListOf<Facility>()
+            val geocoder = Geocoder(context, Locale.KOREA)
+
+            try {
+                val inputStream = context.assets.open("gymData.json")
+                val size = inputStream.available()
+                val buffer = ByteArray(size)
+                inputStream.read(buffer)
+                inputStream.close()
+                val jsonData = String(buffer)
+
+                val jsonArray = JSONArray(jsonData)
+
+                for (i in 0 until jsonArray.length()) {
+                    val facilityObj = jsonArray.getJSONObject(i)
+
+                    val facilityName = facilityObj.getString("시설명")
+                    val sport = facilityObj.getString("종 목")
+                    val address = facilityObj.getString("주소")
+
+                    val addressList = geocoder.getFromLocationName(address, 1)
+                    if (addressList != null && addressList.isNotEmpty()) {
+                        val location = addressList[0]
+                        val latitude = location.latitude
+                        val longitude = location.longitude
+
+                        val facility = Facility(facilityName, sport, address, latitude, longitude)
+                        facilityList.add(facility)
+                    }
+                }
+
+                for (facility in facilityList) {
+                    // 마커 아이콘 생성
+                    val markerDrawable = ContextCompat.getDrawable(context, R.drawable.health)
+                    val markerBitmap = drawableToBitmap(markerDrawable)
+
+                    // 마커 생성
+                    val marker = MapPOIItem()
+                    marker.itemName = facility.name
+                    marker.tag = facilityList.indexOf(facility)
+                    marker.mapPoint = MapPoint.mapPointWithGeoCoord(facility.latitude, facility.longitude)
+                    marker.markerType = MapPOIItem.MarkerType.CustomImage
+                    marker.customImageBitmap = markerBitmap
+
+                    // 마커를 지도에 추가
+                    mapView.addPOIItem(marker)
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
-}
+} 
